b8e684c2a951c55822686c0d94b25cacbfb4509e
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-08-19 14:55:44 +0200

    fix

--- conf/config.php
+++ conf/config.php
@@ -2,14 +2,14 @@
 
 return [
     'base_url' => 'https://__DOMAIN____PATH__', // no trailing slash
+    'storage' => [
+        'driver' => 'local',
+        'path'   => '__INSTALL_DIR__/storage',
+    ],
     'db'       => [
         'connection' => 'sqlite',
         'dsn'        => '__INSTALL_DIR__/resources/database/xbackbone.db',
         'username'   => null,
         'password'   => null,
-    ],
-    'storage' => [
-        'driver' => 'local',
-        'path'   => '__INSTALL_DIR__/storage',
-    ],
+    ]
 ];
